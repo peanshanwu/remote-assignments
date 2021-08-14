eff9e26444cf4e86fcefd7d0743f6055bd8f6121
peanshanwu <62551122+peanshanwu@users.noreply.github.com>
2021-08-15 04:46:25 +0800

    Commit assignment-2 YA!

--- week-3/app.js
+++ week-3/app.js
@@ -1,13 +1,40 @@
 // require the exprees module
 const express = require('express');
-
 // call the express function
 const app = express();
 
-// create a route
-app.get('/', (request, response) => {
-    response.send('express server runs!!');
-})
+
+// body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// set the engine
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+    res.send('express server runs!!');
+});
+
+app.get('/getData', (req, res) => {
+    const { number } = req.query;
+    let message = '';
+    
+    // console.log(number);
+    let total = 0;
+    const r = /^[0-9]*[1-9][0-9]*$/; // 正規式：正整數
+    const integer = r.test(number);  // 回傳true false
+    if (integer) {
+        for (let i = 1; i <= number; i++) {
+            total += i;
+        };
+    } else {
+        message = 'Wrong Parameter';
+    }
+    res.render('getData', { total, message});
+});
+
 
 // set up the express server
 // app.listen(3000);
@@ -15,3 +42,10 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
+
+
+
+
+// -------------------- TO DO -------------------- 
+// 1. 修正數字填入0時的bug
+// 2. 自動填入算式（如輸入3，自動填入 1+2+3=6
