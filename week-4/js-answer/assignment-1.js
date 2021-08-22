2180b884477e04ded2aa5409ea067be5cb709a81
peanshanwu <62551122+peanshanwu@users.noreply.github.com>
2021-08-22 16:40:53 +0800

    Add week-4

---
+++ week-4/js-answer/assignment-1.js
@@ -0,0 +1,15 @@
+function delayedResult(n1, n2, delayTime, callback) {
+  window.setTimeout(callback(n1 + n2), delayTime);
+};
+
+delayedResult(4, 5, 3000, function(result){
+  console.log(result);
+});
+
+delayedResult(-5, 10, 2000, function(result){
+  window.alert(result);
+});
+
+
+
+
